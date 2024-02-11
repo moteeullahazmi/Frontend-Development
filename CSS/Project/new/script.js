100b67226804ff3c014477494463b8ccbdd906f1
Moteeullah Azmi <you@example.com>
2024-02-11 14:18:42 +0530

    Azmi

---
+++ CSS/Project/new/script.js
@@ -0,0 +1,5 @@
+const updateDesignBtn = document.getElementById('update-design-btn');
+
+updateDesignBtn.addEventListener('click', () => {
+  alert('Contact simuliks simuliks-dev@gmail.com to update your design!');
+});
